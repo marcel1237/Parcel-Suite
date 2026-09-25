a63f21bf29df9e31874ff757d98d11eca8ba48eb
Marcel <marcel1237@gmail.com>
2026-09-25 08:30:57 -0300

    Kiwi NG

--- Parcel Play OS/kiwi-build/playos-debian-oracle-uek-live/config/hooks/prep/01-trust.sh
+++ Parcel Play OS/kiwi-build/playos-debian-oracle-uek-live/config/hooks/prep/01-trust.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-# Prep hook to configure apt to allow unauthenticated repositories in chroot
+# Prep hook to configure apt to allow unauthenticated repositories and keys in chroot image-root
 set -e
-if [ -d etc/apt/apt.conf.d ]; then
-    echo 'Acquire::AllowInsecureRepositories "true";' > etc/apt/apt.conf.d/99unauthenticated
-    echo 'Acquire::AllowWeakRepositories "true";' >> etc/apt/apt.conf.d/99unauthenticated
-    echo 'APT::Get::AllowUnauthenticated "true";' >> etc/apt/apt.conf.d/99unauthenticated
-    echo "Configured chroot etc/apt/apt.conf.d/99unauthenticated"
-fi
+for rootdir in output/build/image-root /output/build/image-root /tmp/kiwi-live/output/build/image-root; do
+    if [ -d "$rootdir" ]; then
+        mkdir -p "$rootdir/etc/apt/apt.conf.d" "$rootdir/etc/apt/trusted.gpg.d"
+        echo 'Acquire::AllowInsecureRepositories "true";' > "$rootdir/etc/apt/apt.conf.d/99unauthenticated"
+        echo 'Acquire::AllowWeakRepositories "true";' >> "$rootdir/etc/apt/apt.conf.d/99unauthenticated"
+        echo 'APT::Get::AllowUnauthenticated "true";' >> "$rootdir/etc/apt/apt.conf.d/99unauthenticated"
+        if [ -f /var/cache/kiwi/apt-get/trusted.gpg ]; then
+            cp -f /var/cache/kiwi/apt-get/trusted.gpg "$rootdir/etc/apt/trusted.gpg.d/playos-local.gpg"
+        fi
+        echo "Configured image-root apt security settings at $rootdir"
+    fi
+done
 exit 0
